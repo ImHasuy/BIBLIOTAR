428a63e4ddc5eddce72d6f2cb6fb26444ca0f488
zuzmo00 <safirmohammad33@gmail.com>
2025-04-04 08:34:22 +0200

    Book Entity modified

--- BIBLIOTAR/Entities/Book.cs
+++ BIBLIOTAR/Entities/Book.cs
@@ -5,10 +5,12 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
-        public string Quality {  get; set; }
+        public string Quality { get; set; }
         public string ISBN { get; set; }
-        public string Category {  get; set; }
+        public string Category { get; set; }
         public DateTime PublshDate { get; set; }
-        
+        public enum Status { available, unalvilable }
+
+
     }
 }
--- BIBLIOTAR/obj/Debug/net9.0/BIBLIOTAR.AssemblyInfo.cs
+++ BIBLIOTAR/obj/Debug/net9.0/BIBLIOTAR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiblioTar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c317c4dc0142e162a1c25f23821bae4624b9a4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a49aa880450d6181a4cd407a5571cb5ee9564626")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiblioTar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiblioTar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
