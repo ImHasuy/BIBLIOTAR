d789d4f6626ba3b8b014245d3ab293649399fd27
zuzmo00 <safirmohammad33@gmail.com>
2025-04-14 20:22:02 +0200

    JWT Token Generate

--- BIBLIOTAR/Service/UserService.cs
+++ BIBLIOTAR/Service/UserService.cs
@@ -4,6 +4,11 @@ using BiblioTar.Controllers;
 using BiblioTar.DTOs;
 using BiblioTar.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BiblioTar.Service
 {
@@ -19,6 +24,7 @@ namespace BiblioTar.Service
     {
         private readonly AppDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
         public UserService(AppDbContext context, IMapper mapper)
         {
             _context = context;
@@ -50,5 +56,38 @@ namespace BiblioTar.Service
 
 
         }
+
+        private async Task<string> GenerateToken(User user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["kulcs"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(Convert.ToDouble(5));
+
+            var id = await GetClaimsIdentity(user);
+            var token = new JwtSecurityToken("https://localhost:7017", "https://localhost:7017", id.Claims, expires: expires, signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private async Task<ClaimsIdentity> GetClaimsIdentity(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name), 
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Sid, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.AuthTime, DateTime.Now.ToString(CultureInfo.InvariantCulture))
+            };
+
+            //if (user.Roles != null && user.Roles.Any())
+            //{
+            //    claims.AddRange(user.Roles.Select(role => new Claim("roleIds", Convert.ToString(role.Id))));
+            //    claims.AddRange(user.Roles.Select(role => new Claim(ClaimTypes.Role, role.Name)));
+            //}
+
+            return new ClaimsIdentity(claims, "Token");
+        }
+
     }
 }
