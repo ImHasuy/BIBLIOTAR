7b43903dada2a827be0875f89e4adf80d0b3e002
JanEszter <janovitseszter@gmail.com>
2025-05-06 15:14:49 +0200

    Branch created

--- BIBLIOTAR/Entities/Reservation.cs
+++ BIBLIOTAR/Entities/Reservation.cs
@@ -6,12 +6,10 @@ namespace BiblioTar.Entities
     public class Reservation
     {
         public int Id { get; set; }
-        public string? Email { get; set; } 
-        //Lentebb kifejtve 
-        // ALternalokulcsnak tervezem, egyelore nem lett felepitve a DB contextben
-        //Nem áll egyelőre így semmivel sem kapcsolatban, amennyibben szükséges, hogy valami key legyen, utána nezek
+        public string? Email { get; set; } //Amennyiben a felhasználó nem regisztrált, akkor az email címét itt tároljuk el
+
         [ForeignKey("User")]
-        public int? UserId { get; set; }
+        public int? UserId { get; set; } //Ha a felhasználó nem regisztrált, akkor egy anonymus userhez lesz kapcsolva, és itt lesz eltarolva az email.
         [ForeignKey("Book")]
         public int BookId { get; set; }
         public DateTime ReservationDate { get; set; }
